ad03dd545a40f84a1a8da557517f9a40e9338c32
Phan Thanh Hai <tientam227@gmail.com>
2025-03-27 15:38:04 +0700

    JSON

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,11 +1,22 @@
 import './style.css';
 const Mycomponent = () => {
+    // const Phanhai = "H16"; //string
+    //const Phanhai = 16; //number
+    // const Phanhai = true; //boolean
+    // const Phanhai = [1,2,3]; 
+    const Phanhai = {
+        name: "Phan Hai",
+        age: 16
+    }; //object
     return (
         //fragment<></>
         <>
-            <div>Phan Hai update</div>
+            <div>{JSON.stringify(Phanhai)} Phan Hai update</div>
+            <div>{console.log("ERIC")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}>child</div>
+                style={
+                    { borderRadius: "10px" }
+                }>child</div>
         </>
     );
 }
