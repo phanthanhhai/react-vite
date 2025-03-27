32e38e3229d01f0b44c55b5815fc798990a707ed
Phan Thanh Hai <tientam227@gmail.com>
2025-03-27 14:04:43 +0700

    const

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
+// () => {}
+const App = () => {
   const [count, setCount] = useState(0)
+  // function myFunction() {
+  //   console.log("helllo function")
+  // }
+  // myFunction();
+  const myFunction = () => {
+    console.log("hello function")
+  }
+  myFunction();
 
   return (
     <>
