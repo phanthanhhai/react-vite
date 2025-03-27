141e92548a8bc509334ccde6fb8cef5c3b540a89
Phan Thanh Hai <tientam227@gmail.com>
2025-03-27 15:15:40 +0700

    fragment<></>

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,7 +1,13 @@
+import './style.css';
 const Mycomponent = () => {
-  return (
-    <div>Phan Hai update</div>
-  );
+    return (
+        //fragment<></>
+        <>
+            <div>Phan Hai update</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}>child</div>
+        </>
+    );
 }
 //component = html + css + js
 export default Mycomponent;
