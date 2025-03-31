989dfb5c0a3ecc757ce995e050e52ff7c1a367e6
Phan Thanh Hai <tientam227@gmail.com>
2025-03-31 15:23:43 +0700

    Them

--- src/components/todo/todonew.jsx
+++ src/components/todo/todonew.jsx
@@ -1,11 +1,21 @@
 const TodoNew = (props) => {
-  console.log("check props: ",props)
-  const {addNewTodo} = props;
-  addNewTodo("phan HAii")
-    return(
-        <div className='todo-new'><input type="text" />
-        <button>Add</button>
-      </div>
-    )
+  console.log("check props: ", props)
+  const { addNewTodo } = props;
+  const handClick = () => {
+    alert("click me")
+  }
+  const hendleOnChange = (name) => {
+    console.log("change",name)
+  }
+  return (
+    <div className='todo-new'>
+      <input type="text" 
+      onChange={(event) => hendleOnChange(event.target.value)}
+    />
+      <button style={{ cursor: "pointer" }}
+        onClick={handClick}
+        >Add</button>
+    </div>
+  )
 }
 export default TodoNew;
